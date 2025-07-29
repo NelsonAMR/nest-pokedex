4d97dc173cd667b9ce87d051ec0c2a21e10eef65
Nelson Macias <nelson.macias99@gmail.com>
2025-07-28 23:52:29 -0500

    Creat app config

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,14 @@ import { PokemonModule } from './pokemon/pokemon.module';
 import { MongooseModule } from '@nestjs/mongoose';
 import { CommonModule } from './common/common.module';
 import { SeedModule } from './seed/seed.module';
+import { ConfigModule } from '@nestjs/config';
+import { envConfiguration } from './config/app.config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      load: [envConfiguration],
+    }),
     ServeStaticModule.forRoot({
       rootPath: join(__dirname, '..', 'public'),
     }),
