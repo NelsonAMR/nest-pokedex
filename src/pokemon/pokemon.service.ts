8ae1d09854a02a2b9747e7c8f7cab643b6f585c1
Nelson Macias <nelson.macias99@gmail.com>
2025-07-29 23:23:10 -0500

    use env variables

--- src/pokemon/pokemon.service.ts
+++ src/pokemon/pokemon.service.ts
@@ -10,13 +10,19 @@ import { isValidObjectId, Model } from 'mongoose';
 import { Pokemon } from './entities/pokemon.entity';
 import { InjectModel } from '@nestjs/mongoose';
 import { PaginationDto } from 'src/common/dto/pagination.dto';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class PokemonService {
+  private defaultlimit: number;
+
   constructor(
     @InjectModel(Pokemon.name)
     private readonly pokemonModel: Model<Pokemon>,
-  ) {}
+    private readonly configService: ConfigService,
+  ) {
+    this.defaultlimit = configService.getOrThrow<number>('defaultLimit');
+  }
 
   async create(createPokemonDto: CreatePokemonDto) {
     createPokemonDto.name = createPokemonDto.name.toLowerCase();
@@ -29,7 +35,7 @@ export class PokemonService {
   }
 
   async findAll(paginationDto: PaginationDto) {
-    const { limit = 10, offset = 0 } = paginationDto;
+    const { limit = this.defaultlimit, offset = 0 } = paginationDto;
     return this.pokemonModel
       .find()
       .limit(limit)
