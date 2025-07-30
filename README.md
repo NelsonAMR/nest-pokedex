50cb94ee0ddff17487a8f8144c770514b7efe5aa
Nelson Macias <nelson.macias99@gmail.com>
2025-07-30 16:11:17 -0500

    create schema for env variables

--- README.md
+++ README.md
@@ -23,7 +23,17 @@ npm i -g @nestjs/cli
 docker-compose up -d
 ```
 
-5. Reconstruir base de datos con la semilla
+5. Clonar el archivo **.env.template** y renombar la copia a **.env**
+
+6. Llenar las variables de entorno definidas en el **.env**
+
+7. Ejecutar la aplicacion en dev:
+
+```
+pnpm start:dev
+```
+
+8. Reconstruir base de datos con la semilla
 
 ```
 GET {{API_URI}}/seed
--- src/app.module.ts
+++ src/app.module.ts
@@ -7,11 +7,13 @@ import { CommonModule } from './common/common.module';
 import { SeedModule } from './seed/seed.module';
 import { ConfigModule } from '@nestjs/config';
 import { envConfiguration } from './config/app.config';
+import { joiValidationSchema } from './config/joi.validation';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       load: [envConfiguration],
+      validationSchema: joiValidationSchema,
     }),
     ServeStaticModule.forRoot({
       rootPath: join(__dirname, '..', 'public'),
